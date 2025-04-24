737cefd1857049ecd00c3e86201dc60b5865c746
SumanthKumar007 <vallurusumanthkumar@gmail.com>
2025-04-24 09:43:20 +0530

    added config file

---
+++ config.py
@@ -0,0 +1,9 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+COST_PER_1K_INPUT = 0.000075
+COST_PER_1K_OUTPUT = 0.0003
+CSV_LOG_PATH = "chat_logs.csv"
+MODEL_NAME = "gemini-1.5-pro"
